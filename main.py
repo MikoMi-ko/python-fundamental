70cc7bf76ccb51b5f8674b3cf10da06f60610682
Jatmiko <jatmiko.amungp@gmail.com>
2022-02-12 18:26:23 +0700

    Jokes logika pemprograman

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Semua sintaksis dasar bahasa pemprograman terdiri dari:
+1. Sekuensial: Langkah berurutan.
+2. Percabangan: Langkah melompat jika kondisi terpenuhi.
+3. Perulangan: Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi.
+"""
+
+# Sekuensial
+print('"Budi, belikan ibu sebotol susu, kalo mereka punya telor beli 6 ya"')
+print('"Oke Ibu"')
+print("Budi pergi ke toko dan membeli 6 susu")
+print('"Lhoh ko beli 6 susunya? Ibu kan minta cuma 1 saja"')
+print('"Kan mereka punya telor bu"')
